123a6dd874e6ea08c2d94617a4654169280097cc
Радмир <sch.radmir@yandex.ru>
2022-10-02 22:29:51 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int number = 0;
+        while (number < 10) {
+            number++;
+            System.out.println(number);
+        }
+        for (int i = 10; i > 0; i--) {
+
+            System.out.println(i);
+        }
+        //Задание 2
+        var dayOfReport = 5;
+        while (dayOfReport  < 31) {
+            System.out.println("Сегодня пятница, " + dayOfReport + "-e число. Необходимо подготовить отчет.");
+            dayOfReport = dayOfReport  + 7;
+        }
+        //Задача 3
+        int beginYearOfFlight = 1817;
+        int endYearOfFlight = 2054;
+        int f = 79;
+       while (beginYearOfFlight < endYearOfFlight){
+            beginYearOfFlight = beginYearOfFlight + f;
+            System.out.println(beginYearOfFlight);
+        }
     }
 }
